ea05ce121081eeb18fbd41c1cb9a19709928e836
Phanttoms <yohan.pruche@orange.fr>
2024-02-03 12:33:24 +0100

    header

---
+++ my-app/src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import { Link, NavLink } from "react-router-dom";
+import "../../styles/header.scss";
+import logo from "../../assets/images/logoKasa.png";
+
+function Header() {
+	return (
+		<header className="header">
+			<Link to="/">
+				<img className="header__logo" src={logo} alt="Logo Kasa" />
+			</Link>
+			<nav className="header__navbar">
+				<NavLink className="header__navbar--link" to="/">
+					Accueil
+				</NavLink>
+				<NavLink className="header__navbar--link" to="/about">
+					À propos
+				</NavLink>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
