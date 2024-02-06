318bfd8c6bfd7f76cb7506bd1952e4996c48cf83
Phanttoms <yohan.pruche@orange.fr>
2024-02-06 17:30:26 +0100

    ajout dataAbout

--- my-app/src/components/Collapse/index.jsx
+++ my-app/src/components/Collapse/index.jsx
@@ -1,1 +1,24 @@
 import "../../styles/collapse.scss";
+import { useState } from "react";
+import arrow from "../../assets/vecteur/arrow.svg";
+
+function Collapse({ title, description }) {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<div className="collapse" id={`collapse-${title}`}>
+			<div className="collapse__box">
+				<h2 className="collapse__box--title">Titre</h2>
+				<img
+					src={arrow}
+					alt="Icône de flèche"
+					className={`collapse__box--img ${open}`}
+					onClick={() => setOpen(!open)}
+				/>
+			</div>
+			{open && <div className="collapse__txt">{description}</div>}
+		</div>
+	);
+}
+
+export default Collapse;
--- my-app/src/pages/About/index.jsx
+++ my-app/src/pages/About/index.jsx
@@ -1,8 +1,14 @@
 import Banner from "../../components/Banner";
 import banner from "../../assets/images/bannerImage2.png";
+import Collapse from "../../components/Collapse";
 
 function About() {
-	return <Banner image={banner} />;
+	return (
+		<>
+			<Banner image={banner} />
+			<Collapse />
+		</>
+	);
 }
 
 export default About;
