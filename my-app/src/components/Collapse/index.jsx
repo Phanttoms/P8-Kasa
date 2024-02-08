0fd416434d18bd0f72dc522a8f756c4aac534668
Phanttoms <yohan.pruche@orange.fr>
2024-02-08 15:04:50 +0100

    smooth collapse

--- my-app/src/components/Collapse/index.jsx
+++ my-app/src/components/Collapse/index.jsx
@@ -4,6 +4,10 @@ import arrow from "../../assets/icones/arrow.svg";
 function Collapse({ title, description }) {
 	const [open, setOpen] = useState(false);
 
+	const toggleCollapse = () => {
+		setOpen(!open);
+	};
+
 	return (
 		<div className="collapse" id={`collapse-${title}`}>
 			<div className="collapse__box">
@@ -12,10 +16,12 @@ function Collapse({ title, description }) {
 					src={arrow}
 					alt="Icône de flèche"
 					className={`collapse__box--img ${open}`}
-					onClick={() => setOpen(!open)}
+					onClick={toggleCollapse}
 				/>
 			</div>
-			{open && <div className="collapse__txt">{description}</div>}
+			<div className={`collapse__txt ${open}`}>
+				<p className={`collapse__txt--txt ${open}`}>{description}</p>
+			</div>
 		</div>
 	);
 }
