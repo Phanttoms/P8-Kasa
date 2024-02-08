9f499f10ab0603651c488a98bad82ac006b6c7b6
Phanttoms <yohan.pruche@orange.fr>
2024-02-08 18:25:03 +0100

    collapse modif

--- my-app/src/components/Banner/index.jsx
+++ my-app/src/components/Banner/index.jsx
@@ -2,7 +2,7 @@ function Banner({ image, title }) {
 	return (
 		<div className="banner">
 			<img className="banner__img" src={image} alt="Bannière du site Kasa" />
-			<h1 className="banner__txt">{title}</h1>
+			{!title ? "" : <h1 className="banner__txt">{title}</h1>}
 		</div>
 	);
 }
